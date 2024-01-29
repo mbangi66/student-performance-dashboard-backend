99a192133e31bd91c32d5523e2e4a260ab95e190
Mustaqeem <mbangi66@gmail.com>
2024-01-29 18:34:13 +0300

    Middleware for parsing JSON requests and Middleware for handling CORS

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const port = process.env.PORT || 3000;
 // Connect to the SQLite database
 const db = new sqlite3.Database('student_perf.db')
 
+// Middleware for parsing JSON requests
+app.use(express.json());
+
+// Middleware for handling CORS (Cross-Origin Resource Sharing)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
+
